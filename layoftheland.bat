963dc47d021c7ee87676bc4b744660ba3150890d
durban11 <danau@ieee.org>
2017-04-14 13:38:11 -0400

    Create layoftheland.bat

---
+++ layoftheland.bat
@@ -0,0 +1,9 @@
+:: Run systeminfo and pipe the output to findstr looking for key values
+systeminfo | findstr /B /C:"OS Name" /C:"OS Version"
+:: hostname of the system I am on ?
+echo hostname: 
+hostname
+:: and who am i running as ?
+echo user: %username%
+:: and lastly a bit more about the users
+net users
