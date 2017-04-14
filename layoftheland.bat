a1901060cd6abb47456dab5a97e65bf1e79def87
durban11 <danau@ieee.org>
2017-04-14 13:41:09 -0400

    Update layoftheland.bat

--- layoftheland.bat
+++ layoftheland.bat
@@ -1,3 +1,7 @@
+:: Dana Urban
+:: go get the lay of the land with some basic information
+::
+::
 :: Run systeminfo and pipe the output to findstr looking for key values
 systeminfo | findstr /B /C:"OS Name" /C:"OS Version"
 :: hostname of the system I am on ?
